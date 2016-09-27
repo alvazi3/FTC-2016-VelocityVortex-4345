cb90eb993028e4a7445650b5fad18940c876301d
Chris Xiong <chrisxiongyang@yahoo.com>
2016-09-26 21:31:14 -0700

    Created drivetrain class for 2-motor non-encoded tank-drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+public class Drivetrain {
+
+    private DcMotor leftDriveMotor, rightDriveMotor;
+
+    public Drivetrain(DcMotor leftDriveMotor, DcMotor rightDriveMotor) {
+        this.leftDriveMotor = leftDriveMotor;
+        this.rightDriveMotor = rightDriveMotor;
+
+        this.rightDriveMotor.setDirection(DcMotor.Direction.REVERSE);
+        this.rightDriveMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        leftDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void tankDrive(double leftPower, double rightPower) {
+        leftDriveMotor.setPower(leftPower);
+        rightDriveMotor.setPower(rightPower);
+    }
+
+    public void arcadeDrive(double power, double turn) {
+        double leftPower = Range.clip(power - turn, -1, 1);
+        double rightPower = Range.clip(power + turn, -1, 1);
+
+        leftDriveMotor.setPower(leftPower);
+        rightDriveMotor.setPower(rightPower);
+    }
+}
