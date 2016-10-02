6814b0ec44a0fb68f265164f9e39ca007fc8691b
Miles P. Olson <mpolson64@gmail.com>
2016-10-02 13:03:02 -0700

    Added features for usability (LED control, raw output)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/BeaconActivator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/BeaconActivator.java
@@ -10,17 +10,44 @@ public class BeaconActivator {
     private DeviceInterfaceModule cdim;
 
     private int ledChannel;
+    private boolean isLedLit;
 
     public BeaconActivator(ColorSensor rgbSensor, DeviceInterfaceModule cdim, int ledChannel) {
         this.rgbSensor = rgbSensor; 
         this.cdim = cdim;
         this.ledChannel = ledChannel;
 
+        isLedLit = false;
+
         cdim.setDigitalChannelMode(this.ledChannel, DigitalChannelController.Mode.OUTPUT);
+        cdim.setDigitalChannelState(this.ledChannel, isLedLit);
     }
 
-    public boolean isRed(){
+    public boolean isRed() {
         return rgbSensor.red() > rgbSensor.blue();
     }
 
+    public void setLed(boolean lit) {
+        isLedLit = lit;
+        cdim.setDigitalChannelState(ledChannel, isLedLit);
+    }
+
+    public boolean isLedLit() {
+        return isLedLit;
+    }
+
+    public boolean toggleLed() {
+        cdim.setDigitalChannelState(ledChannel, !isLedLit);
+        isLedLit = !isLedLit;
+        return isLedLit;
+    }
+
+    public int[] getRawColors() {
+        return new int[]{
+            rgbSensor.alpha(),
+            rgbSensor.red(),
+            rgbSensor.green(),
+            rgbSensor.blue()
+        };
+    }
 }
