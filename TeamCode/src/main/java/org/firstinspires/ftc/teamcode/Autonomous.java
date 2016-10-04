cface042e7de8b76c3ad0e23348abc989983d980
zacharythomasabraham <zachary.thomas.abraha@gmail.com>
2016-10-03 23:57:46 -0700

    Adding Autonomous main page with Beacon Subsysteming Other systems like pushing the ball or computer visions can be accessed in this class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.subsystems.*;
+/**
+ * Created by zacharyabraham on 10/3/16.
+ */
+
+public class Autonomous extends OpMode {
+    private BeaconActivator beaconActivator;
+
+    @Override
+    public void init(){
+        beaconActivator = new BeaconActivator(hardwareMap.colorSensor.get("color_sensor"), hardwareMap.deviceInterfaceModule.get("cdim"), 15 );
+    }
+    public void loop(){
+    }
+}
