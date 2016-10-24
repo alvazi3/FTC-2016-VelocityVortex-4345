c433e90e53ba63bf4e99874197bc782ddb5f4351
Zachary Abraham <zachary.thomas.abraham@gmail.com>
2016-10-24 15:45:23 -0700

    Here is the new code for the catapult.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ChooChoo.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+/**
+ * Created by member on 10/17/16.
+ */
+public class ChooChoo {
+
+    private DcMotor chooChooMotor;
+
+    public ChooChoo(DcMotor chooChooMotor){
+        this.chooChooMotor = chooChooMotor;
+        this.chooChooMotor.setDirection(DcMotor.Direction.FORWARD);
+        chooChooMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void catapultBall(double motorPower){
+        chooChooMotor.setPower(motorPower);
+    }
+    public void stop(){
+
+    }
+
+}
