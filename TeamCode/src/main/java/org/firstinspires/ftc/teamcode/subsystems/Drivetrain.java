0241e526dddbe2c68337ba3c4a0da50fe594bc57
Abraham <zachary.thomas.abraham@gmail.com>
2016-12-01 14:25:12 -0800

    Added encoder code to right side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -20,7 +20,7 @@ public class Drivetrain {
         this.rightDriveMotor.setDirection(DcMotor.Direction.REVERSE);
 
         leftDriveMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDriveMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void tankDrive(double leftPower, double rightPower) {
@@ -39,36 +39,40 @@ public class Drivetrain {
         tankDrive(0, 0);
     }
 
-    public void resetEncoder() {
+    public void resetEncoders() {
         leftDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftDriveMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDriveMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public int getPosition() {
-        return leftDriveMotor.getCurrentPosition();
+    public int[] getPosition() {
+        int[] output = {leftDriveMotor.getCurrentPosition(), rightDriveMotor.getCurrentPosition()};
+        return output;
     }
 
     public void driveTo(double position) {
+        resetEncoders();
         double rotations = position / (WHEEL_DIAMETER * Math.PI);
 
         arcadeDrive(-0.25, 0);
 
-        while(rotations * TICKS_PER_ROTATION - Math.abs(leftDriveMotor.getCurrentPosition()) > ACCEPTABLE_THRESHOLD);
+        while(rotations * TICKS_PER_ROTATION - Math.abs(leftDriveMotor.getCurrentPosition()) > ACCEPTABLE_THRESHOLD && rotations * TICKS_PER_ROTATION - Math.abs(rightDriveMotor.getCurrentPosition()) > ACCEPTABLE_THRESHOLD);
 
         stop();
     }
 
     public void turnTo(double angle) {
+        resetEncoders();
         double rotations = (BOT_DIAMETER / WHEEL_DIAMETER) * (-1 * angle / 360);
 
         if (angle > 0) {
             tankDrive(-0.5, 0.5);
-        }
-        else {
+        } else {
             tankDrive(0.5, -0.5);
         }
 
-        while(Math.abs(rotations * TICKS_PER_ROTATION - leftDriveMotor.getCurrentPosition()) > ACCEPTABLE_THRESHOLD);
+        while(Math.abs(rotations * TICKS_PER_ROTATION - leftDriveMotor.getCurrentPosition()) > ACCEPTABLE_THRESHOLD && rotations * TICKS_PER_ROTATION - Math.abs(rightDriveMotor.getCurrentPosition()) > ACCEPTABLE_THRESHOLD);
 
         stop();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShootingAndDriveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShootingAndDriveAuto.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.LED;
 
 @Autonomous(name="Unnamed4345: Shooting/Drive Auto", group="unnamed4345")
 public class ShootingAndDriveAuto extends LinearOpMode {
@@ -10,6 +12,11 @@ public class ShootingAndDriveAuto extends LinearOpMode {
     private ChooChoo chooChoo;
     private Drivetrain drivetrain;
     private Intake intake;
+
+    private ColorSensor rgbSensor;
+    private LED onboardLed;
+    private DeviceInterfaceModule cdim;
+    private Servo buttonPusher;
 
     public void initialize() {
         drivetrain = new Drivetrain(hardwareMap.dcMotor.get("left_drive"), hardwareMap.dcMotor.get("right_drive"));
@@ -44,7 +51,13 @@ public class ShootingAndDriveAuto extends LinearOpMode {
         while(timer.seconds() < 2 && opModeIsActive());
 
         intake.rotate(0.25);
-        drivetrain.tankDrive(-1, -0.9);
+        drivetrain.driveTo(60);
+        //Hit the Ball
+        drivetrain.turnTo(-90);
+        //Turn to Beacon
+        drivetrain.driveTo(24);
+        //Drive Toward Beacon
+        drivetrain.BeaconActivator(rgbSensor, cdim, onboardLed, buttonPusher);
 
         timer.reset();
         while (timer.seconds() < 3 && opModeIsActive());
