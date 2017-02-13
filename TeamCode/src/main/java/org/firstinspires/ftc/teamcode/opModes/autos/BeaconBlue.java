5663178a5973295db29a8466c578902062e62f38
Miles P. Olson <Miles P. Olson>
2017-02-12 17:35:08 -0800

    Competition commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/autos/BeaconBlue.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.opModes.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.subsystems.BeaconActivator;
+import org.firstinspires.ftc.teamcode.subsystems.Catapult;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+
+@Autonomous(name="legend27: Beacon Red", group="legend27")
+public class BeaconBlue extends LinearOpMode {
+
+    private boolean teamRed = true;
+
+    private ElapsedTime timer;
+
+    private Catapult catapult;
+    private Drivetrain drivetrain;
+    private Intake intake;
+    private BeaconActivator beaconActivator;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        timer = new ElapsedTime();
+
+        drivetrain = new Drivetrain(hardwareMap.dcMotor.get("left_drive"), hardwareMap.dcMotor.get("right_drive"));
+        intake = new Intake(hardwareMap.dcMotor.get("intake"));
+        catapult = new Catapult(hardwareMap.dcMotor.get("catapult"));
+        beaconActivator = new BeaconActivator(hardwareMap.colorSensor.get("sensor"), hardwareMap.deviceInterfaceModule.get("dim"), hardwareMap.led.get("led"), hardwareMap.dcMotor.get("button_pusher"));
+
+        waitForStart();
+
+        drivetrain.tankDrive(1, 1);
+        telemetry.addData("Status", "Driving forward0");
+        timer.reset();
+        while (timer.seconds() < 1 && opModeIsActive());
+
+        if(teamRed) {
+            drivetrain.tankDrive(0.125, -0.125);
+        }
+        else {
+            drivetrain.tankDrive(-0.125, 0.125);
+        }
+        telemetry.addData("Status", "Turning");
+
+        timer.reset();
+        while (timer.seconds() < 2.45 && opModeIsActive());
+
+        drivetrain.tankDrive(.2, .2);
+        telemetry.addData("Status", "Driving forward1 ");
+        timer.reset();
+        while (timer.seconds() < 3 && opModeIsActive());
+
+        drivetrain.stop();
+
+        beaconActivator.doBeacon(teamRed);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOps/TeleOpSingleNOCOLORSENSOR.java
@@ -0,0 +1,117 @@
+package org.firstinspires.ftc.teamcode.opModes.teleOps;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.*;
+
+@TeleOp(name="legend27: TeleOp Single", group="legend27")
+public class TeleOpSingleNOCOLORSENSOR extends OpMode {
+
+    private Catapult catapult;
+    private Drivetrain drivetrain;
+    private Intake intake;
+    //private BeaconActivator beaconActivator;
+    
+    @Override
+    public void init() {
+        drivetrain = new Drivetrain(hardwareMap.dcMotor.get("left_drive"), hardwareMap.dcMotor.get("right_drive"));
+        intake = new Intake(hardwareMap.dcMotor.get("intake"));
+        catapult = new Catapult(hardwareMap.dcMotor.get("catapult"));
+        //beaconActivator = new BeaconActivator(hardwareMap.colorSensor.get("sensor"), hardwareMap.deviceInterfaceModule.get("dim"), hardwareMap.led.get("led"), hardwareMap.dcMotor.get("button_pusher"));
+    }
+
+    @Override
+    public void start() {
+        drivetrain.stop();
+        intake.stop();
+        catapult.stop();
+        //beaconActivator.stop();
+    }
+
+    @Override
+    public void loop() {
+        drivetrain.arcadeDrive(-1 * gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+        intakeControls();
+        catapultControls();
+        //beaconActivatorControls();
+        telemetry.addData("Catapult Position", catapult.getPosition());
+        telemetry.addData("Drive Position", drivetrain.getPosition()[0] + ", " + drivetrain.getPosition()[1]);
+        //telemetry.addData("Red", beaconActivator.getRawColors()[1]);
+        //telemetry.addData("Green", beaconActivator.getRawColors()[2]);
+        //telemetry.addData("Blue", beaconActivator.getRawColors()[3]);
+    }
+
+    public void stop() {
+        drivetrain.stop();
+        intake.stop();
+        catapult.stop();
+    }
+
+    private void catapultControls() {
+        if (gamepad1.x) {
+            catapult.rotate();
+            while(gamepad1.x);
+
+        }
+        else if (gamepad1.y) {
+            catapult.catapultBall(1, 1);
+            while (gamepad1.y);
+        }
+        else if (gamepad1.right_bumper) {
+            catapult.holdPosition();
+            while (gamepad1.right_bumper);
+        }
+        else if (gamepad1.back) {
+            catapult.resetEncoder();
+        }
+        else {
+            if (!catapult.isHolding()) {
+                catapult.holdPosition();
+            }
+        }
+
+    }
+
+    private void intakeControls() {
+        if (gamepad1.b) {
+            intake.rollIn();
+        }
+        else if (gamepad1.a) {
+            intake.rollOut();
+        }
+
+        else if (gamepad1.right_trigger > 0.1) {
+            try {
+                intake.rotate(1);
+            } catch (InterruptedException e) {
+
+            }
+
+            while (gamepad1.right_trigger > 0.1);
+        }
+
+        else {
+            intake.stop();
+        }
+    }
+    /*
+    private void beaconActivatorControls() {
+        if (gamepad1.left_bumper) {
+            beaconActivator.push();
+        }
+        else if (gamepad1.left_trigger > 0.1) {
+            beaconActivator.pull();
+        }
+        else if (gamepad1.dpad_up) {
+            beaconActivator.pushButton();
+        }
+        else {
+            beaconActivator.stop();
+        }
+
+    }
+    */
+
+}
